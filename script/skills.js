059a369055e344f7d5e847116cf01971d489161f
MBrayan25 <micael.brayan.s@gmail.com>
2022-12-25 18:02:47 -0300

    Refatorei o arquivo skills.js
    Separei em um arquivo o array que continha as configurações dos elementos e o importei para skills.js

--- script/skills.js
+++ script/skills.js
@@ -1,43 +1,7 @@
-const skills = [
-    {
-        iconClass: 'fa-brands fa-html5',
-        name: 'HTML5',
-        pContent: '- Estrutura <br>- Semântica <br>'
-    },
-    {
-        iconClass: 'fa-brands fa-css3',
-        name: 'CSS3',
-        pContent: '- Responsividade <br> - Animações <br>'
-    },
-    {
-        iconClass: 'fa-brands fa-square-js',
-        name: 'JavaScript',
-        pContent: "- Clean code <br> - POO <br> - API's <br> - Design patters básicos"
-    },
-    {
-        iconClass: 'fa-brands fa-square-js',
-        name: 'Jquery',
-        pContent: '- Manipulação'
-    },
-    {
-        iconClass: 'fa-brands fa-sass',
-        name: 'SASS',
-        pContent: '- Organização <br> - Compacto <br> - Responsividade <br> - Animações <br>'
-    },
-    {
-        iconClass: 'fa-brands fa-git',
-        name: 'Git',
-        pContent: '- Commits limpos <br> - Branches <br> - Mergers'
-    },
-    {
-        iconClass: 'fa-brands fa-github',
-        name: 'Github',
-        pContent: '- Git remote'
-    }
-]
+import dataSkills from './data/skills.js'
 
 function handleSkills(){
-    skills.forEach(skill => skillElementConstructor(skill))
+    dataSkills.forEach(skill => skillElementConstructor(skill))
 }
 
 function skillElementConstructor({ iconClass, name, pContent }){
