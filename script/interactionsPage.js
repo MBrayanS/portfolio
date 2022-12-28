f2701c8dc44709753fea16ee88cc8bf8bf876289
MBrayan25 <micael.brayan.s@gmail.com>
2022-12-28 17:16:30 -0300

    Coloquei a função que mostra o .sidebar num arquivo separado
    Criei o arquivo interactionsPage.js, rescrevi nele a função do menu lateral e a exportei para o main.js

---
+++ script/interactionsPage.js
@@ -0,0 +1,4 @@
+export function showSidebar(){
+  $('.hamburguer_btn').toggleClass('hamburguer_btn--rotate')
+  $('.sidebar').toggleClass('sidebar--show')
+}
--- script/main.js
+++ script/main.js
@@ -1,10 +1,8 @@
+import { showSidebar } from './interactionsPage.js'
 import handleSkills from './skills.js' 
 import handleProjects from './projects.js'
 
-$('.hamburguer_btn').click(function(){
-    $(this).toggleClass('hamburguer_btn--rotate')
-    $('.sidebar').toggleClass('sidebar--show')
-});
+$('.hamburguer_btn').click(showSidebar);
 
 handleSkills()
 handleProjects()
