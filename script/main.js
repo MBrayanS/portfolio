6193fda0443fa4bcdd7f96bd9205ee7e716c524c
MBrayanS <micael.brayan.s@gmail.com>
2023-10-10 19:34:50 -0300

    FEAT: Zerei o projeto
    Removi todos os arquivos e resetei as configurações parar começar a pagina do zero.
    
    Deixei instalado o npm com o node-sass para a compilação dos arquivos scss.

--- script/main.js
+++
@@ -1,8 +0,0 @@
-import { showSidebar } from './interactionsPage.js'
-import handleSkills from './skills.js' 
-import handleProjects from './projects.js'
-
-$('.hamburguer_btn').click(showSidebar);
-
-handleSkills()
-handleProjects()
