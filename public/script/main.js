e319c762fa154a2d3b652f7b96ed1804d794fc57
MBrayanS <micael.brayan.s@gmail.com>
2023-10-13 13:16:40 -0300

    FEAT: Edite a regra de negócio do menu
    Para que o menu abra automaticamente nas telas maiores, mas não nas mobile.
    
    E para que ao clicar fora do menu ele se feche.

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,27 @@
 const menuDOM = document.querySelector('#menu')
+const htmlDOM = document.querySelector('html')
+
+destravarMenu()
 
 menuDOM.addEventListener('click', esconderMostrarMenu)
+htmlDOM.addEventListener('click', esconderMenuSeClicarFora)
 
-function esconderMostrarMenu() { menuDOM.classList.toggle('menu--invisivel') }
+function esconderMostrarMenu() {
+    menuDOM.classList.toggle('menu--invisivel')
+    menuDOM.classList.remove('menu--travado')
+}
+
+function esconderMenuSeClicarFora({ target }) {
+    const clicouForaDoMenu = !menuDOM.contains(target)
+    
+    if( clicouForaDoMenu ) menuDOM.classList.add('menu--invisivel')
+}
+
+function destravarMenu() {
+    const larguraDaTelaMaiorQue800 = document.body.clientWidth > 800
+
+    if( larguraDaTelaMaiorQue800 ) { 
+        menuDOM.classList.remove('menu--travado')
+        menuDOM.classList.remove('menu--invisivel')
+    }
+}
