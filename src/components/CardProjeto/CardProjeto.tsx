28a8c931bc41c0d931737f5cb66ea3d70abc9843
MBrayanS <micael.brayan.s@gmail.com>
2025-06-01 22:51:05 -0300

    FEAT: Atualização na bio

--- src/components/CardProjeto/CardProjeto.tsx
+++ src/components/CardProjeto/CardProjeto.tsx
@@ -1,55 +1,66 @@
-import './CardProjeto.scss'
+import "./CardProjeto.scss";
 
-import { IProjeto } from '../../interfaces'
+import { IProjeto } from "../../interfaces";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import listaDeTecnologias from '../../data/tecnologias'
+import listaDeTecnologias from "../../data/tecnologias";
 
-const imagemNaoCarregada = './img/icons/icon-not-found.svg'
+const imagemNaoCarregada = "./img/icons/icon-not-found.svg";
 
-export default function CardProjeto( props: IProjeto ) {
-    const { nome, descricao, tecnologias, linkDaImagem, linkDoDeploy, linkDoRepo } = props
+export default function CardProjeto(props: IProjeto) {
+	const {
+		nome,
+		descricao,
+		tecnologias,
+		linkDaImagem,
+		linkDoDeploy,
+		linkDoRepo,
+	} = props;
 
-    const [ caminhoDaImagem, setCaminhoDaImagem ] = useState(linkDaImagem)
+	const [caminhoDaImagem, setCaminhoDaImagem] = useState(linkDaImagem);
 
-    const soHaUmButton = linkDoDeploy && linkDoRepo ? false : true
+	const soHaUmButton = linkDoDeploy && linkDoRepo ? false : true;
 
-    const renderizarBtn = ( link: string, texto: string ) => {
-        return <a rel='noopener' href={link} target="_blank"> <p>{texto}</p> <span className='background'></span> </a>
-    }
+	const renderizarBtn = (link: string, texto: string) => {
+		return (
+			<a rel="noopener" href={link} target="_blank">
+				{" "}
+				<p>{texto}</p> <span className="background"></span>{" "}
+			</a>
+		);
+	};
 
-    const onError = () => setCaminhoDaImagem(imagemNaoCarregada)
+	const onError = () => setCaminhoDaImagem(imagemNaoCarregada);
 
-    return <div className="card-projeto">
+	return (
+		<div className="card-projeto">
+			<div className="img-projeto">
+				<a rel="noopener" href={linkDoDeploy} target="_blank">
+					<img src={caminhoDaImagem} alt={nome} onError={onError} />
+				</a>
+			</div>
 
-        <div className="img-projeto">
-            <a rel='noopener' href={linkDoDeploy} target="_blank">
-                <img src={caminhoDaImagem} alt={nome} onError={onError} />
-            </a>
-        </div>
+			<h3>{nome}</h3>
 
-        <h3>{nome}</h3>
+			<p>{descricao}</p>
 
-        <p>{descricao}</p>
+			<div className="tecnologias">
+				{tecnologias.map((nome: string) => {
+					const { imagem } = listaDeTecnologias[nome];
 
-        <div className="tecnologias">{
+					return (
+						<div className="card-tecnologia" key={nome}>
+							<img src={imagem} alt={nome} />
+						</div>
+					);
+				})}
+			</div>
 
-            tecnologias.map( ( nome: string ) => {
-                const { imagem } = listaDeTecnologias[nome]
-                
-                return <div className="card-tecnologia" key={nome} >
-                    <img src={imagem} alt={nome}/>
-                </div>
-
-            })
-
-        }</div>
-
-        <div className={ 'btns' + ( soHaUmButton ? ' btns--um-btn' : '' ) }>
-            { linkDoDeploy && renderizarBtn(linkDoDeploy, 'Deploy') }
-            { linkDoRepo && renderizarBtn(linkDoRepo, 'Código') }
-        </div>
-
-    </div>
+			<div className={"btns" + (soHaUmButton ? " btns--um-btn" : "")}>
+				{linkDoDeploy && renderizarBtn(linkDoDeploy, "Deploy")}
+				{linkDoRepo && renderizarBtn(linkDoRepo, "Código")}
+			</div>
+		</div>
+	);
 }
--- src/components/RedesSociais/RedesSociais.tsx
+++ src/components/RedesSociais/RedesSociais.tsx
@@ -1,9 +1,6 @@
 import './RedesSociais.scss'
 
 export default function RedesSociais() {
-    const linkDoInstagram = 'https://www.instagram.com/m_brayan_dev/'
-    const iconDoInstagram = 'img/icons/icon-rede-social-instagram.svg'
-
     const linkDoLinkedin = 'https://www.linkedin.com/in/mbrayans/'
     const iconDoLinkedin = 'img/icons/icon-rede-social-linkedin.svg'
 
@@ -12,10 +9,6 @@ export default function RedesSociais() {
     
     return <aside className="redes-sociais">
             
-        <a className='rede' rel='noopener' href={linkDoInstagram} target="_blank">
-            <img src={iconDoInstagram} alt="Intagram" />
-        </a>
-
         <a className='rede' rel='noopener' href={linkDoLinkedin} target="_blank">
             <img src={iconDoLinkedin} alt="LinkedIn" />
         </a>
